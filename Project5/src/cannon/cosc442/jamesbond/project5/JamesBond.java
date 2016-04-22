54fc4cb18265fcc000731cdb304eb3be545d4ba5
ncanno1 <ncanno1@students.towson.edu>
2016-04-22 19:18:45 -0400

    Change the original FSM file for task 3 to create new tests

--- Project5/src/cannon/cosc442/jamesbond/project5/JamesBond.java
+++ Project5/src/cannon/cosc442/jamesbond/project5/JamesBond.java
@@ -1,7 +1,9 @@
 package cannon.cosc442.jamesbond.project5;
 
+import java.util.regex.Pattern;
+
 public class JamesBond {
 	public boolean bondRegex(String input) {
-		return false;
+		return input.matches("[0123456789)]*([123456789()]*007[0123456789(]*)[0123456789()]*");
 	}
 }
